f69b957076ca6743b738c6a417cdfe0b1c42acbb
mila-zh <105249447+mila-zh@users.noreply.github.com>
2022-12-02 21:16:54 -0500

    basics

--- AWSLab4/AWSLab4/Function.cs
+++ AWSLab4/AWSLab4/Function.cs
@@ -19,7 +19,7 @@ public class Function
     /// <summary>
     /// The default minimum confidence used for detecting labels.
     /// </summary>
-    public const float DEFAULT_MIN_CONFIDENCE = 70f;
+    public const float DEFAULT_MIN_CONFIDENCE = 90f;
 
     /// <summary>
     /// The name of the environment variable to set which will override the default minimum confidence level.
